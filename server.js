025132ebc9f3becf060ad4b8f5b88a1bced5f818
Jakobi <jakobioommen@yahoo.com>
2025-10-16 17:57:51 -0500

    port 5000 was blocked

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 const instructorRoutes = require("./routes/instructorRoutes.cjs")
 const classRoutes = require("./routes/classRoutes.cjs")
 require("dotenv").config();
@@ -10,8 +11,7 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
-
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 //routes
 app.use("/api/instructor", instructorRoutes);
@@ -23,6 +23,10 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error", err));
 
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "public", "index.html"))
+);
+
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
